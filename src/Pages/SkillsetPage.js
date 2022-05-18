714d97868b8608c0969399ef7ce2d6021ed967d1
Mohsin Sayed <mohsinsayed1999@gmail.com>
2022-05-18 07:42:11 +0530

    Skillset Section structure of Skillset Page Section

---
+++ src/Pages/SkillsetPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// importing styles
+import "../Styles/SkillsetPage.css";
+
+class SkillsetPage extends React.Component {
+  render() {
+    return (
+      <div className="SkillsetPageContainer">
+        <h1>SKILLS</h1>
+      </div>
+    );
+  }
+}
+
+export default SkillsetPage;
