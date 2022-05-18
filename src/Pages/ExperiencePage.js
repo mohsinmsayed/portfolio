a81a276044569b4d0226ed04fee6106503af0efe
Mohsin Sayed <mohsinsayed1999@gmail.com>
2022-05-18 07:41:39 +0530

    Experience Section structure of Experience Page Section

---
+++ src/Pages/ExperiencePage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// importing styles
+import "../Styles/ExperiencePage.css";
+
+class ExperiencePage extends React.Component {
+  render() {
+    return (
+      <div className="ExperiencePageContainer">
+        <span>Experience</span>
+        <div className="ExperienceMainContainer">
+          <div className="ExperienceContentContainer">
+            <span>LTI</span>
+            <span>Engineer - Cloud & Infra Services</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ExperiencePage;
