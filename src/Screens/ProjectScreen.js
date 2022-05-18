2103ab58861132e9c85e100aa3ca37378dc128e7
Mohsin Sayed <mohsinsayed1999@gmail.com>
2022-05-18 07:52:23 +0530

    Project Screen basic structure of project screen

--- src/Screens/ProjectScreen.js
+++ src/Screens/ProjectScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+// importing styles
+import "../Styles/ProjectScreen.css";
+
 class ProjectScreen extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Project Screen</h1>
+      <div className="ProjectScreenContainer">
+        <h1>MY PROJECTS</h1>
+        <div className="ProjectsContainer"></div>
       </div>
     );
   }
